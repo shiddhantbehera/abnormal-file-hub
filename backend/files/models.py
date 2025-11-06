6d1685e3346035f9e95ed64606cdf933c7e2f2e2
Shiddhant Behera <shiddhant1601@gmail.com>
2025-11-06 23:03:39 +0530

    updated file table - models and migration

--- backend/files/models.py
+++ backend/files/models.py
@@ -10,14 +10,29 @@ def file_upload_path(instance, filename):
 
 class File(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    file = models.FileField(upload_to=file_upload_path)
-    original_filename = models.CharField(max_length=255)
-    file_type = models.CharField(max_length=100)
-    size = models.BigIntegerField()
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(upload_to=file_upload_path, null=True, blank=True)
+    original_filename = models.CharField(max_length=255, db_index=True)
+    file_type = models.CharField(max_length=100, db_index=True)
+    size = models.BigIntegerField(db_index=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    
+    # Deduplication fields
+    file_hash = models.CharField(max_length=64, db_index=True, null=True, blank=True)
+    reference_count = models.IntegerField(default=1)
+    is_duplicate = models.BooleanField(default=False)
+    original_file = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='duplicates'
+    )
     
     class Meta:
         ordering = ['-uploaded_at']
+        indexes = [
+            models.Index(fields=['file_hash', 'is_duplicate']),
+        ]
     
     def __str__(self):
         return self.original_filename
